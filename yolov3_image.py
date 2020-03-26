36a8c1449e1691b0d6f067d977e9b34e7d39387d
Sevoflurane <ilya.terminator.27@gmail.com>
2020-03-26 19:13:21 +0300

    Files updated

--- yolov3_image.py
+++ yolov3_image.py
@@ -20,8 +20,8 @@ COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
 	dtype="uint8")
 
 # derive the paths to the YOLO weights and model configuration
-weightsPath = os.path.sep.join(['yolo-coco', "yolov3.weights"])
-configPath = os.path.sep.join(['yolo-coco', "yolov3.cfg"])
+weightsPath = os.path.sep.join(['yolo-coco/weights', "yolov3.weights"])
+configPath = os.path.sep.join(['yolo-coco/cfg', "yolov3.cfg"])
 
 # load our YOLO object detector trained on COCO dataset (80 classes)
 print("[INFO] loading YOLO from disk...")
