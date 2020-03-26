a66e1524559154ee8c41a7aab7a25f5948eaa179
Sevoflurane <ilya.terminator.27@gmail.com>
2020-03-26 21:35:22 +0300

    Safe Area script added

---
+++ yolov3_camera_SaFe_Territory.py
@@ -0,0 +1,191 @@
+# import the necessary packages
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+import os
+import time
+import datetime
+h_NN = 192
+w_NN = 192
+
+h_show = 416*2
+w_show = 256*2
+
+thr_param = 0.3  # threshold when applying non-maxima suppression
+conf_param = 0.5  # minimum probability to filter weak detections
+
+warn_img = 'warning.png'
+warn_image = cv2.imread(warn_img)
+(H, W) = warn_image.shape[:2]
+warn_image = cv2.resize(warn_image,(h_show//2,w_show//2))
+
+
+class FPS:
+    def __init__(self):
+        # store the start time, end time, and total number of frames
+        # that were examined between the start and end intervals
+        self._start = None
+        self._end = None
+        self._numFrames = 0
+
+    def start(self):
+        # start the timer
+        self._start = datetime.datetime.now()
+        return self
+
+    def stop(self):
+        # stop the timer
+        self._end = datetime.datetime.now()
+
+    def update(self):
+        # increment the total number of frames examined during the
+        # start and end intervals
+        self._numFrames += 1
+
+    def elapsed(self):
+        # return the total number of seconds between the start and
+        # end interval
+        return (self._end - self._start).total_seconds()
+
+    def fps(self):
+        # compute the (approximate) frames per second
+        return self._numFrames / self.elapsed()
+
+
+# load the COCO class labels our YOLO model was trained on
+labelsPath = os.path.sep.join(['yolo-coco', "coco.names"])
+LABELS = open(labelsPath).read().strip().split("\n")
+
+# initialize a list of colors to represent each possible class label
+np.random.seed(42)
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+                           dtype="uint8")
+
+# derive the paths to the YOLO weights and model configuration
+weightsPath = os.path.sep.join(['yolo-coco/weights', "yolov3.weights"])
+configPath = os.path.sep.join(['yolo-coco/cfg', "yolov3.cfg"])
+
+# load our YOLO object detector trained on COCO dataset (80 classes)
+# and determine only the *output* layer names that we need from YOLO
+print("[INFO] loading YOLO from disk...")
+net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+# initialize the video stream, pointer to output video file, and
+# frame dimensions
+vs = cv2.VideoCapture(0)
+fps = FPS().start()
+
+writer = None
+(W, H) = (None, None)
+
+# loop over frames from the video file stream
+while True:
+    time0 = time.time()
+    # read the next frame from the file
+    (grabbed, frame) = vs.read()
+
+    # if the frame was not grabbed, then we have reached the end
+    # of the stream
+    if not grabbed:
+        break
+    # frame = imutils.resize(frame, width=96)
+
+    # # if the frame dimensions are empty, grab them
+    if W is None or H is None:
+        (H, W) = frame.shape[:2]
+
+    # construct a blob from the input frame and then perform a forward
+    # pass of the YOLO object detector, giving us our bounding boxes
+    # and associated probabilities
+    time2 = time.time()
+
+    blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (h_NN, w_NN),
+                                 swapRB=True, crop=False)
+    net.setInput(blob)
+    start = time.time()
+    layerOutputs = net.forward(ln)
+    end = time.time()
+
+    # initialize our lists of detected bounding boxes, confidences,
+    # and class IDs, respectively
+    boxes = []
+    confidences = []
+    classIDs = []
+
+    # loop over each of the layer outputs
+    for output in layerOutputs:
+        # loop over each of the detections
+        for detection in output:
+            # extract the class ID and confidence (i.e., probability)
+            # of the current object detection
+            scores = detection[5:]
+            classID = np.argmax(scores)
+            confidence = scores[classID]
+
+            # filter out weak predictions by ensuring the detected
+            # probability is greater than the minimum probability
+            if confidence > conf_param and (classID == 0 or classID == 2):
+                # scale the bounding box coordinates back relative to
+                # the size of the image, keeping in mind that YOLO
+                # actually returns the center (x, y)-coordinates of
+                # the bounding box followed by the boxes' width and
+                # height
+                box = detection[0:4] * np.array([W, H, W, H])
+                (centerX, centerY, width, height) = box.astype("int")
+
+                # use the center (x, y)-coordinates to derive the top
+                # and and left corner of the bounding box
+                x = int(centerX - (width / 2))
+                y = int(centerY - (height / 2))
+
+                # update our list of bounding box coordinates,
+                # confidences, and class IDs
+                boxes.append([x, y, int(width), int(height)])
+                confidences.append(float(confidence))
+                classIDs.append(classID)
+
+    # apply non-maxima suppression to suppress weak, overlapping
+    # bounding boxes
+    idxs = cv2.dnn.NMSBoxes(boxes, confidences, conf_param,
+                            thr_param)
+
+    # ensure at least one detection exists
+    if len(idxs) > 0:
+        # loop over the indexes we are keeping
+        for i in idxs.flatten():
+            # extract the bounding box coordinates
+            (x, y) = (boxes[i][0], boxes[i][1])
+            (w, h) = (boxes[i][2], boxes[i][3])
+
+            # draw a bounding box rectangle and label on the frame
+            color = [int(c) for c in COLORS[classIDs[i]]]
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            text = "{}: {:.4f}".format(LABELS[classIDs[i]],
+                                       confidences[i])
+            cv2.putText(frame, text, (x, y - 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    time3 = time.time()
+
+    # Display the resulting frame
+    frame = cv2.resize(frame,(h_show,w_show))
+    if len(idxs) > 0:
+        # warn_image=cv2.addWeighted(frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:],0.4,warn_image,0.1,0)
+        frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:] = np.where(warn_image>70,frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:], warn_image)
+        # frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:] = warn_image
+        # frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:] = warn_image
+    cv2.imshow('Frame', frame)
+
+    time4 = time.time()
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    fps.update()
+# stop the timer and display FPS information
+fps.stop()
+print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+# release the file pointers
+print("[INFO] cleaning up...")
