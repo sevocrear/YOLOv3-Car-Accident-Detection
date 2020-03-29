b5e2058d5017277d180c8bb200b25b95d34a7ed7
albertdemian <albert.mte@gmail.com>
2020-03-29 14:47:29 +0300

    made modifications of files to run over multiple folders

---
+++ vehicle_tracking.py
@@ -0,0 +1,81 @@
+import numpy as np
+from numpy import linalg as LA
+
+def  centroid(box):
+  #box = [x,y,w,h]
+  centroids= []
+  for i in box:
+    dis_x = np.int32(i[2]/2)
+    dis_y = np.int32(i[3]/2)
+    centroid = [i[0]+dis_x, i[1]+dis_y]
+    centroids.append(centroid)
+  return centroids
+
+
+def get_closest_center(old_center, new_centers):
+  centers_distance = [[],[]]
+  for i in new_centers: 
+    motion_vector = np.subtract(i,old_center)
+    distance = np.sqrt(motion_vector[0]**2 + motion_vector[1]**2)
+    normal = LA.norm(motion_vector)
+    motion_vector = np.divide(motion_vector,normal)
+    centers_distance[0].append([motion_vector])
+    centers_distance[1].append(distance)
+  
+  
+  min_idx = centers_distance[1].index(min(centers_distance[1]))
+
+  return new_centers[min_idx], centers_distance[0][min_idx], centers_distance[1][min_idx], min_idx
+
+
+
+def update_dict(arrays,new_center,vector,distance ): 
+  arrays[0].append(new_center)
+  arrays[1].append(vector)
+  arrays[2].append(distance)
+  
+ 
+  if len(arrays[2])<3:
+    arrays[3].append(0)
+  else: 
+    accel = arrays[2][len(arrays[2])-2]-distance
+    arrays[3].append(accel)
+
+  return arrays
+
+
+def BuildAndUpdate(boxes, cars_dict):
+  centers = centroid(boxes)
+  if len(cars_dict)==0:
+    for i in range(len(centers)):
+      car_info = [[]for i in range(4)]
+      label = str(i+1)
+      car_info[0].append(centers[i])
+      car_info[1].append([0,0])
+      car_info[2].append(0)
+      car_info[3].append(0)
+      cars_dict[label]= car_info
+  else:
+    cars_labels = list(cars_dict) 
+    for i in cars_labels:
+      if len(centers)>0:
+        locations = cars_dict[i][0]
+        old_center = locations[len(locations)-1]
+        closest_center, vector, distance, idx = get_closest_center(old_center,centers)
+        car_bounds = boxes[idx][2:4]
+        if distance <= min(car_bounds)/1.5:
+          cars_dict[i]= update_dict(cars_dict[i],closest_center, vector, distance)
+          del centers[idx]
+    
+    int_labels = []
+    for car_label in cars_labels:
+      int_labels.append(int(car_label))
+   
+    if len(centers)> 0:
+      for center in centers:
+        new_label = str(max(int_labels)+1)
+        cars_dict[new_label] = [[center],[[0,0]],[0],[0]]
+
+        
+
+  return cars_dict
---
+++ yolov3_frames.py
@@ -0,0 +1,243 @@
+# This code implements YOLOv3 technique in order to detect car accidents on the video frames. 
+# If you want to use this with camera, you can easily modify it
+
+# import the necessary packages
+import numpy as np
+import argparse
+import time
+import cv2
+import os
+import matplotlib.pyplot as plt
+import glob
+from vehicle_tracking import * # functions for tracking
+
+thr_param = 0.3
+conf_param = 0.5
+data_dir = "Dataset/"
+dataset_path = glob.glob(data_dir+"*/")
+print('path',dataset_path)
+for path in dataset_path:
+	split_path = path.split('/')
+	print('sp',split_path)
+	folders = glob.glob(path)
+	print('folder',folders)
+	img_dir = split_path[1] # Enter Directory of all images 
+	data_path = os.path.join(path,'*g')
+
+	frame_counter = len(glob.glob(data_path))
+	print(data_path)
+	print(frame_counter)
+
+	files = []
+	#for q in range(frame_counter):
+	for q in range(10):
+		path = folders[0]+'/'+str(q)+'.jpg'
+		files.append(path)
+
+	print(files)
+
+	cars_dict = {}
+	counter = 1
+	for f1 in files:
+		image = cv2.imread(f1)
+		print(type(image))
+		print(str(counter)+'/'+str(frame_counter))
+		counter +=1
+
+		if type(image) is np.ndarray:	
+			# load the COCO class labels our YOLO model was trained on
+			labelsPath = os.path.sep.join(['yolo-coco', "coco.names"])
+			LABELS = open(labelsPath).read().strip().split("\n")
+
+			# initialize a list of colors to represent each possible class label
+			np.random.seed(42)
+			COLORS = np.random.randint(0, 255, size=(len(LABELS), 3),
+				dtype="uint8")
+
+			# derive the paths to the YOLO weights and model configuration
+			weightsPath = os.path.sep.join(['yolo-coco/weights', "yolov3.weights"])
+			configPath = os.path.sep.join(['yolo-coco/cfg', "yolov3.cfg"])
+
+			# load our YOLO object detector trained on COCO dataset (80 classes)
+			print("[INFO] loading YOLO from disk...")
+			net = cv2.dnn.readNetFromDarknet(configPath, weightsPath)
+
+
+			# load our input image and grab its spatial dimensions
+			#image = cv2.imread(img)
+			(H, W) = image.shape[:2]
+
+			# determine only the *output* layer names that we need from YOLO
+			ln = net.getLayerNames()
+			ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+			# construct a blob from the input image and then perform a forward
+			# pass of the YOLO object detector, giving us our bounding boxes and
+			# associated probabilities
+			blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (96, 96),
+				swapRB=True, crop=False)
+			net.setInput(blob)
+			start = time.time()
+			layerOutputs = net.forward(ln)
+			end = time.time()
+
+			# show timing information on YOLO
+			print("[INFO] YOLO took {:.6f} seconds".format(end - start))
+
+			# initialize our lists of detected bounding boxes, confidences, and
+			# class IDs, respectively
+			boxes = []
+			confidences = []
+			classIDs = []
+
+			# loop over each of the layer outputs
+			for output in layerOutputs:
+				# loop over each of the detections
+				for detection in output:
+					# extract the class ID and confidence (i.e., probability) of
+					# the current object detection
+					scores = detection[5:]
+					classID = np.argmax(scores)
+					confidence = scores[classID]
+
+					# filter out weak predictions by ensuring the detected
+					# probability is greater than the minimum probability
+					if confidence > conf_param and (classID == 0 or classID == 2):
+						# scale the bounding box coordinates back relative to the
+						# size of the image, keeping in mind that YOLO actually
+						# returns the center (x, y)-coordinates of the bounding
+						# box followed by the boxes' width and height
+						box = detection[0:4] * np.array([W, H, W, H])
+						(centerX, centerY, width, height) = box.astype("int")
+
+						# use the center (x, y)-coordinates to derive the top and
+						# and left corner of the bounding box
+						x = int(centerX - (width / 2))
+						y = int(centerY - (height / 2))
+
+						# update our list of bounding box coordinates, confidences,
+						# and class IDs
+						boxes.append([x, y, int(width), int(height)])
+						confidences.append(float(confidence))
+						classIDs.append(classID)
+
+			# apply non-maxima suppression to suppress weak, overlapping bounding
+			# boxes
+			idxs = cv2.dnn.NMSBoxes(boxes, confidences, conf_param,
+				thr_param)
+
+			# ensure at least one detection exists
+			if len(idxs) > 0:
+				# loop over the indexes we are keeping
+				new_boxes = []
+				for i in idxs.flatten():
+					new_boxes.append(boxes[i]) 
+					# extract the bounding box coordinates
+					
+				#print(new_boxes)
+				cars_dict = BuildAndUpdate(new_boxes, cars_dict)
+				cars_labels = list(cars_dict)
+				for i in idxs.flatten():
+					(x, y) = (boxes[i][0], boxes[i][1])
+					(w, h) = (boxes[i][2], boxes[i][3])
+					# draw a bounding box rectangle and label on the image
+					color = [int(c) for c in COLORS[classIDs[i]]]
+					cv2.rectangle(image, (x, y), (x + w, y + h), color, 1)
+					text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
+					cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+						0.5, color, 1)
+				
+				for car_label in cars_labels:
+					car_path = cars_dict[car_label][0]
+					#print('p',car_path)
+					if len(car_path)> 1:
+						car_path = np.asarray(car_path,dtype=np.int32)
+						car_path = car_path.reshape((-1,1,2))
+						cv2.polylines(image,car_path,True,(0,0,255))
+						label_location = car_path[len(car_path)-1][0]
+						#print(label_location)
+						cv2.putText(image, car_label, (label_location[0], label_location[1]), cv2.FONT_HERSHEY_SIMPLEX,
+						0.5, color, 1)
+						
+
+				
+
+			# show the output image
+			#cv2.imshow("Image", image)
+			#time.sleep(2)
+			#cv2.waitKey(0)
+			print(classIDs)
+
+	#cv2.waitKey(0)
+	print(cars_dict)
+	print(list(cars_dict))
+	cv2.imwrite('output/'+img_dir+'_final_frame.png', image)
+	cars_plot_data = {}
+	for label in cars_labels: 
+		position  = cars_dict[label][0]
+		direction = cars_dict[label][1]
+		velocity = cars_dict[label][2]
+		acceleration = cars_dict[label][3]
+		x_pos = []
+		y_pos = []
+		angle = []
+		time_frame = []
+		cars_plot_data[label]={}
+		for i in range(len(position)):
+			x_pos.append(position[i][0])
+			y_pos.append(position[i][1])
+			angle.append(np.arccos(direction[0]))
+			time_frame.append(i)
+
+		cars_plot_data[label]['x'] = x_pos
+		cars_plot_data[label]['y'] = y_pos
+		cars_plot_data[label]['time'] = time_frame
+		cars_plot_data[label]['angle'] = angle
+		cars_plot_data[label]['velocity'] = velocity
+		cars_plot_data[label]['acceleration'] = acceleration
+
+	plt.figure(figsize=(10,8))
+	for label in cars_labels: 
+		plt.plot(cars_plot_data[label]['x'],cars_plot_data[label]['y'])
+	plt.legend(cars_labels,loc='center left', bbox_to_anchor=(1, 0.5))
+	plt.xlabel('position x')
+	plt.ylabel('position y')
+	plt.xlim((0,image.shape[0]))
+	plt.ylim((0,image.shape[1]))
+	plt.title('cars trajectories')
+	plt.savefig('figures/'+img_dir+'_trajectory.png')
+
+
+
+	plt.figure(figsize=(10,8))
+	plt.subplots_adjust(wspace=0.5)
+	plt.subplot(221)
+	for label in cars_labels: 
+		plt.plot(cars_plot_data[label]['time'],cars_plot_data[label]['angle'])
+	plt.legend(cars_labels,loc='center left', bbox_to_anchor=(1, 0.5))
+	plt.xlabel('frame')
+	plt.ylabel('angle (rad)')
+	plt.title('cars angles')
+
+
+	plt.subplot(222)
+	for label in cars_labels: 
+		plt.plot(cars_plot_data[label]['time'],cars_plot_data[label]['velocity'])
+	plt.legend(cars_labels,loc='center left', bbox_to_anchor=(1, 0.5))
+	plt.xlabel('frame')
+	plt.ylabel('velocity (pixel/frame)')
+	plt.title('cars velocities')
+
+
+	plt.subplot(223)
+	for label in cars_labels: 
+		plt.plot(cars_plot_data[label]['time'],cars_plot_data[label]['acceleration'])
+	plt.legend(cars_labels,loc='center left', bbox_to_anchor=(1, 0.5))
+	plt.xlabel('frame')
+	plt.ylabel(r'acceleration (pixel/${frame}^2$)')
+	plt.title('cars accelerations')
+
+	plt.savefig('figures/'+img_dir+'_Info.png')
+	#plt.show()
+	#cv2.waitKey(0)
+	cv2.destroyAllWindows()
