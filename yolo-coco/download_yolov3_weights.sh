220ce16d21d1b35b3b8aa354a2313c8553ce263a
Ilia Sevostianov <ilya.terminator.27@gmail.com>
2020-03-30 13:49:43 +0300

    Added commands for dataset download

--- yolo-coco/download_yolov3_weights.sh
+++ yolo-coco/download_yolov3_weights.sh
@@ -1,10 +1,12 @@
 # !/bin/bash
 cd ..
-printf "Creating needed folders ...\n"
-mkdir -p Dataset figures output && cd yolo-coco
+echo "Creating needed folders ...\n"
+mkdir -p figures output
+axel -a https://drive.google.com/drive/folders/1mg4_nvXmSbLX0uSLD7YPrN6GtnCEJVv-?usp=sharing
+cd yolo-coco
 # make '/weights' directory if it does not exist and cd into it
 mkdir -p weights && cd weights
-printf "Copying weight files into yolo-coco/weights folder... It will take time ...\n"
+echo "Copying weight files into yolo-coco/weights folder... It will take time ...\n"
 # copy darknet weight files, continue '-c' if partially downloaded
 sudo apt-get install axel
 axel -a https://pjreddie.com/media/files/yolov3.weights
