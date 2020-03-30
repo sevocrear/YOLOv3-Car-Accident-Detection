f40c1e88cc1bff9e03c354e1f65f2cce35fe1917
Ilia Sevostianov <ilya.terminator.27@gmail.com>
2020-03-30 12:49:39 +0300

    Files fixed

--- yolo-coco/download_yolov3_weights.sh
+++ yolo-coco/download_yolov3_weights.sh
@@ -1,10 +1,12 @@
 # !/bin/bash
-
+cd ..
+printf "Creating needed folders ...\n"
+mkdir -p Dataset figures output && cd yolo-coco
 # make '/weights' directory if it does not exist and cd into it
 mkdir -p weights && cd weights
-
+printf "Copying weight files into yolo-coco/weights folder... It will take time ...\n"
 # copy darknet weight files, continue '-c' if partially downloaded
 sudo apt-get install axel
-axel https://pjreddie.com/media/files/yolov3.weights
-axel https://pjreddie.com/media/files/yolov3-tiny.weights
-axel https://pjreddie.com/media/files/yolov3-spp.weights
+axel -a https://pjreddie.com/media/files/yolov3.weights
+axel -a https://pjreddie.com/media/files/yolov3-tiny.weights
+axel -a https://pjreddie.com/media/files/yolov3-spp.weights
