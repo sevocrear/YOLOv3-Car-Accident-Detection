456c947300e056c1b414bb037275b1199f3d61fe
Ilia Sevostianov <ilya.terminator.27@gmail.com>
2020-03-30 15:38:23 +0300

    Files changed

--- yolo-coco/download_yolov3_weights.sh
+++ yolo-coco/download_yolov3_weights.sh
@@ -1,12 +1,18 @@
 # !/bin/bash
+# This bash-script downloads weights and dataset. Also, it creates all needed files
 cd ..
-echo "Creating needed folders ...\n"
+echo "Creating needed folders ..."
 mkdir -p figures output
-axel -a https://drive.google.com/drive/folders/1mg4_nvXmSbLX0uSLD7YPrN6GtnCEJVv-?usp=sharing
+echo "Downloading needed datasets..."
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1w4dkVx03hSe3G-cwItDFTDPLqPBWuBhO' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1w4dkVx03hSe3G-cwItDFTDPLqPBWuBhO" -O Dataset.zip && rm -rf /tmp/cookies.txt
+echo "Unzipping Dataset folder ..."
+sudo apt install unzip
+unzip Dataset.zip
+rm Dataset.zip
 cd yolo-coco
 # make '/weights' directory if it does not exist and cd into it
 mkdir -p weights && cd weights
-echo "Copying weight files into yolo-coco/weights folder... It will take time ...\n"
+echo "Copying weight files into yolo-coco/weights folder... It will take time ..."
 # copy darknet weight files, continue '-c' if partially downloaded
 sudo apt-get install axel
 axel -a https://pjreddie.com/media/files/yolov3.weights
