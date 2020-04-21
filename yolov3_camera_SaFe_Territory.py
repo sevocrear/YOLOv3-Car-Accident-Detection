987fac7020c3eec608c2a843606bff5ca766c39d
Sevoflurane <ilya.terminator.27@gmail.com>
2020-04-21 16:06:13 +0300

    code fixed

--- yolov3_camera_SaFe_Territory.py
+++ yolov3_camera_SaFe_Territory.py
@@ -31,38 +31,6 @@ warn_image = cv2.imread(warn_img)
 warn_image = cv2.resize(warn_image,(h_show//2,w_show//2))
 
 
-class FPS:
-    def __init__(self):
-        # store the start time, end time, and total number of frames
-        # that were examined between the start and end intervals
-        self._start = None
-        self._end = None
-        self._numFrames = 0
-
-    def start(self):
-        # start the timer
-        self._start = datetime.datetime.now()
-        return self
-
-    def stop(self):
-        # stop the timer
-        self._end = datetime.datetime.now()
-
-    def update(self):
-        # increment the total number of frames examined during the
-        # start and end intervals
-        self._numFrames += 1
-
-    def elapsed(self):
-        # return the total number of seconds between the start and
-        # end interval
-        return (self._end - self._start).total_seconds()
-
-    def fps(self):
-        # compute the (approximate) frames per second
-        return self._numFrames / self.elapsed()
-
-
 # load the COCO class labels our YOLO model was trained on
 labelsPath = os.path.sep.join(['yolo-coco', "coco.names"])
 LABELS = open(labelsPath).read().strip().split("\n")
@@ -86,14 +54,13 @@ ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 # initialize the video stream, pointer to output video file, and
 # frame dimensions
 vs = cv2.VideoCapture(0)
-fps = FPS().start()
 
 writer = None
 (W, H) = (None, None)
 
 # loop over frames from the video file stream
 while True:
-    time0 = time.time()
+    time_start = time.time()
     # read the next frame from the file
     (grabbed, frame) = vs.read()
 
@@ -110,14 +77,12 @@ while True:
     # construct a blob from the input frame and then perform a forward
     # pass of the YOLO object detector, giving us our bounding boxes
     # and associated probabilities
-    time2 = time.time()
 
     blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (h_NN, w_NN),
-                                 swapRB=True, crop=False)
+                                swapRB=True, crop=False)
     net.setInput(blob)
     start = time.time()
     layerOutputs = net.forward(ln)
-    end = time.time()
 
     # initialize our lists of detected bounding boxes, confidences,
     # and class IDs, respectively
@@ -137,7 +102,7 @@ while True:
 
             # filter out weak predictions by ensuring the detected
             # probability is greater than the minimum probability
-            if confidence > conf_param and (classID == 0 or classID == 2):
+            if confidence > conf_param and (classID == 0):
                 # scale the bounding box coordinates back relative to
                 # the size of the image, keeping in mind that YOLO
                 # actually returns the center (x, y)-coordinates of
@@ -174,27 +139,18 @@ while True:
             color = [int(c) for c in COLORS[classIDs[i]]]
             cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
             text = "{}: {:.4f}".format(LABELS[classIDs[i]],
-                                       confidences[i])
+                                    confidences[i])
             cv2.putText(frame, text, (x, y - 5),
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-    time3 = time.time()
 
     # Display the resulting frame
     frame = cv2.resize(frame,(h_show,w_show))
     if len(idxs) > 0:
-        # warn_image=cv2.addWeighted(frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:],0.4,warn_image,0.1,0)
         frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:] = np.where(warn_image>70,frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:], warn_image)
-        # frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:] = warn_image
-        # frame[frame.shape[0]//4: frame.shape[0]//4+warn_image.shape[0], frame.shape[1]//4: frame.shape[1]//4+warn_image.shape[1],:] = warn_image 
     cv2.imshow('Frame', frame)
-
-    time4 = time.time()
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    time_end = time.time()
+    print('FPS = ', 1/(time_end - time_start))
+    if cv2.waitKey(1) == 27: # if esc button clicked, then exit loop
         break
-    fps.update()
-# stop the timer and display FPS information
-fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-# release the file pointers
-print("[INFO] cleaning up...")
+
+print('Finished! you\'re awesome')    
