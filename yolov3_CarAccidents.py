467fd60294f6270b017183fbda9a1b9941583a1f
Sevoflurane <ilya.terminator.27@gmail.com>
2020-04-23 14:03:05 +0300

    Files updated

--- yolov3_CarAccidents.py
+++ yolov3_CarAccidents.py
@@ -42,15 +42,19 @@ os.chdir(os.path.dirname(path_of_file))
 
 thr_param = 0.3 # threshold for YOLO detection
 conf_param = 0.5 # confidence for YOLO detection
-frame_start_with = 140
-frame_end_with = 170 # number of image frames to work with in each folder (dataset)
+frame_start_with = 1
+frame_end_with = 160 # number of image frames to work with in each folder (dataset)
 
 filter_flag = 1 # use moving averaging filter or not (1-On, 0 - Off)
 len_on_filter = 2 # minimum length of the data list to apply filter on it
 
 T_var = 200 # threshold in order to show only those cars that is moving... (50 is okay)
+
 k_overlap = 0.25 # ratio (0-1) for overlapping issue | thresh_overlap = distance_between*k_overlap
 frame_overlapped_interval = 20 # the interval (- frame_overlapped_interval + frame; frame + frame_overlapped_interval) to analyze if there were accident or not
+
+T_acc = 1.5 # theshold in order to detect acceleration anomaly
+
 
 
 data_dir = "Dataset/" 	#dataset directory
@@ -88,8 +92,10 @@ for path in dataset_path: # Loop through folders with different video frames (si
 	update_times = 1
 	cars_dict = {}
 	counter = 1
+	images_saved = []
 	for f1 in files:
 		image = cv2.imread(f1)
+		images_saved.append(image)
 		print('Processing frame:'+str(counter)+'/'+str(frame_counter),'in folder:'+folders[0]+'...')
 
 		if type(image) is np.ndarray:	
@@ -200,7 +206,7 @@ for path in dataset_path: # Loop through folders with different video frames (si
 
 	#saving output image in folder output/
 	cv2.imwrite('output/'+img_dir+'_final_frame.png', image)
-
+	cv2.destroyAllWindows()
 	#building dictionary of plot data
 	cars_data = {}
 	for label in cars_labels: 
@@ -280,8 +286,9 @@ for path in dataset_path: # Loop through folders with different video frames (si
 		cars_data[label]['velocity'] = interp_points[:,4]
 		cars_data[label]['acceleration'] = interp_points[:,5]
 
+	checks = [0,0,0]
 
-	# Check vehicle overlaps:
+	#------Checking vehicle overlapps--------#
 	overlapped = set()
 	flag = 1
 	frames = [int(i) for i in range(frame_end_with - frame_start_with)]
@@ -291,18 +298,68 @@ for path in dataset_path: # Loop through folders with different video frames (si
 				if (int(second_car) != int(first_car)) and (check_overlap((cars_data[first_car]['x'][frame],cars_data[first_car]['y'][frame]),(cars_data[second_car]['x'][frame],cars_data[second_car]['y'][frame]), cars_data[first_car]['car diagonal'], cars_data[second_car]['car diagonal'],k_overlap)):
 					overlapped.add(first_car)
 					overlapped.add(second_car)
-					print(first_car, second_car)
 					if flag:
 						frame_overlapped = frame
 						flag = 0	
-	print('labels of overlapped cars:', overlapped,'. Frame of potential accident:', frame_overlapped)
-
+	if not flag:					
+		print('labels of overlapped cars:', overlapped,'. Frame of potential accident:', frame_overlapped)
+		checks[0] = 1
+	else:
+		print('There weren\'nt any accidents this time... Happily...')
+		exit()
 	potential_cars_labels = [label for label in overlapped]
 				
+	#------Checking acceleration anomaly--------#
+	'''When two vehicles are overlapping, we find the acceleration of the vehicles from their speeds captured in the
+	dictionary. We find the average acceleration of the vehicles
+	for N frames before the overlapping condition and the
+	maximum acceleration of the vehicles N frames after it.
+	We find the change in accelerations of the individual vehicles
+	by taking the difference of the maximum acceleration and
+	average acceleration during overlapping condition'''
+
+	frames_before = [int(i) for i in range(frame_overlapped-frame_overlapped_interval, frame_overlapped)]
+
+	acc_average = []
+	for label in potential_cars_labels:
+		acc_av = 0
+		t = 1
+		for frame in frames_before:
+			acc_av = acc_av*(t-1)/t + cars_data[label]['acceleration'][frame]/t
+			t += 1
+		acc_average.append(acc_av)
+	acc_maximum = []
+	for label in potential_cars_labels:
+		acc_max = 0
+		for frame in frames_before:
+			if cars_data[label]['acceleration'][frame]>acc_max:
+				acc_max = cars_data[label]['acceleration'][frame]
+		acc_maximum.append(acc_max)
+
+	acc_diff = np.mean(np.subtract(acc_maximum, acc_average))
+
+	if acc_diff > T_acc:
+		checks[1] = 1
+	else:
+		checks[0] = 0.5
+
+	#----Angle Anomalies----#
 
 
+	#----Checkings----#
+	if (checks[0]+checks[1] + checks[2])>1.5:
+		image = images_saved[frame_overlapped]
+		cv2.imshow('Car Accident',image)
+		if cv2.waitKey(1) == 27:
+				break
+		time.sleep(3)
+		print('accident happened at frame ',frame_overlapped,' between cars ', overlapped)
+		for car_label in potential_cars_labels:
+			cv2.circle(image, (int(cars_data[car_label]['x'][frame_overlapped]), int(cars_data[car_label]['y'][frame_overlapped])), 50,  (255,255,0), 2)
 
-
+		cv2.putText(image, 'frame ' + str(frame_overlapped), (20, image.shape[0]-20), cv2.FONT_HERSHEY_SIMPLEX, 3, (255,255,200), 2)
+		#saving output image in folder output/
+		cv2.imwrite('cars/accident.png', image)			
 
 
 	#-----# Plots			
@@ -316,7 +373,6 @@ for path in dataset_path: # Loop through folders with different video frames (si
 	X = np.arange(0, W, W//10)
 	Y = np.arange(0, W, W//10)
 	X, Y = np.meshgrid(X, Y)
-	print(len(X))
 	Z = np.full((10,1),frame_overlapped)
 	ax.plot_wireframe(X, Y, Z)
 	Z = np.full((10,1),frame_overlapped - frame_overlapped_interval)
