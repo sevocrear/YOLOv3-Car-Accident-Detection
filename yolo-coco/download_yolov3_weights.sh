b999a6a9366e471d5f1b6bd85ba3983e57e95446
Sevoflurane <ilya.terminator.27@gmail.com>
2020-04-25 22:19:30 +0300

    bash changed

--- yolo-coco/download_yolov3_weights.sh
+++ yolo-coco/download_yolov3_weights.sh
@@ -4,7 +4,7 @@ cd ..
 echo "Creating needed folders ..."
 mkdir -p figures output cars
 echo "Downloading needed datasets..."
-wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1JjggC0TSYrQMEkTVo8n5RgjtlweuOwyp' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1JjggC0TSYrQMEkTVo8n5RgjtlweuOwyp" -O Dataset.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=1TioFKqtfM_VSB6pESLUm5GiUgJLZBTZR' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1TioFKqtfM_VSB6pESLUm5GiUgJLZBTZR" -O Dataset.zip && rm -rf /tmp/cookies.txt
 echo "Unzipping Dataset folder ..."
 sudo apt install unzip
 unzip Dataset.zip
@@ -15,6 +15,4 @@ mkdir -p weights && cd weights
 echo "Copying weight files into yolo-coco/weights folder... It will take time ..."
 # copy darknet weight files, continue '-c' if partially downloaded
 sudo apt-get install axel
-axel -a https://pjreddie.com/media/files/yolov3.weights
-axel -a https://pjreddie.com/media/files/yolov3-tiny.weights
-axel -a https://pjreddie.com/media/files/yolov3-spp.weights
+wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://drive.google.com/uc?export=download&id=18PtBISXPhT9CLKy0i5gc8YmrH3Fl1KMj' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=18PtBISXPhT9CLKy0i5gc8YmrH3Fl1KMj" -O yolov3.weights && rm -rf /tmp/cookies.txt
